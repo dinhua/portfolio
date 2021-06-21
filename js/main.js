e62ee8a87dad72608a8482eeffbb6d1368d892f3
Fay Sie <sel12342001@gmail.com>
2021-06-21 18:05:47 +0800

    update general mobile style

--- js/main.js
+++ js/main.js
@@ -7,4 +7,25 @@ $(function(){
             document.body.classList.remove('loading');
         })
     }, 1000);
+
+    // 抓取目標按鈕
+    var $btn = $('.cta-button');
+      
+    // 儲存目標按鈕的值
+    var $desktopText = $btn.html();
+    var $mobileText = $btn.attr('title');
+      
+    if ($(window).width() < 1024) {
+        $btn.html($mobileText);
+    } else {
+        $btn.html($desktopText);
+    }
+      
+    $(window).resize(function () {
+        if ($(window).width() < 1024) {
+            $btn.html($mobileText);
+        } else {
+            $btn.html($desktopText);
+        }
+    });
 });
